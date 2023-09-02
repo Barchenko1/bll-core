627fde261ca74e4b8307d549ab8f64e5ffd0617f
Pavel Barchenko <PAVEL_BARCHENKO@homedepot.com>
2023-09-02 16:39:05 +0200

    fix tests datasets

--- src/test/java/com/bll/core/JUnitTestSuite.java
+++ src/test/java/com/bll/core/JUnitTestSuite.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
         "com.bll.core.bean.constant",
         "com.bll.core.bean.tenant.create",
         "com.bll.core.bean.tenant.update",
+        "com.bll.core.bean.tenant.delete",
         "com.bll.core.bean.org.create",
         "com.bll.core.bean.transaction"})
 class JUnitTestSuite {
